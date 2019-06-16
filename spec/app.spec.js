bd621cfdc67f1df40617d592894c12e712edaf41
Sem - 6 - INGENIERIA INDUSTRIAL: Wilson Alejandro Gonzalez Gaitan <wilsongonzalez.ag@academia.umb.edu.co>
2019-06-16 13:49:20 -0500

    Implements test to check function about jasmine

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,5 +1,33 @@
-const saludar = require('../app');
+const saludar = require("../app");
 
-it('La función saluda', () => {
-  expect(saludar('Platzi')).toBe('Hola Platzi');
+var x = true;
+
+const letra = "AB";
+
+var tmp = undefined;
+
+describe("Prueba de test para string", () => {
+  it("La función saluda", () => {
+    expect(saludar("Platzi")).toBe("Hola Platzi");
+  });
+
+  it("Comprobar valores booleanos", () => {
+    expect(x).toBeTruthy();
+  });
+
+  it("Comprobar valores iguales", () => {
+    expect(x).toEqual(true);
+  });
+
+  it("Comprobar valores iguales", () => {
+    expect(x).toBe(true);
+  });
+
+  it("Comprobar valores de expresiones regulares", () => {
+    expect(x).toMatch("[^AB]");
+  });
+
+  it("Comprobar valores undefined", () => {
+    expect(tmp).toBeUndefined();
+  });
 });
